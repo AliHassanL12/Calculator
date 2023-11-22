ed89983bd5056e3e58a6c8cb55f0c84d47656518
Ali Hassan Liaquat <liaqqahl@gmail.com>
2023-11-22 00:42:10 +0000

    Add operations

---
+++ javascript.js
@@ -0,0 +1,34 @@
+function addition(a,b){
+    return a+b;
+}
+
+function subtraction(a,b){
+    return a-b;
+}
+
+function multiplication(a,b){
+    return a*b;
+}
+
+function division(a,b){
+    return a/b;
+}
+
+let firstNumber, operator, secondNumber;
+
+function operate(num1, num2, operator){
+    if (operator == "+"){
+        return addition(num1,num2);
+    }
+    else if (operator == "-"){
+        return subtraction(num1,num2);
+    }
+    else if (operator == "*"){
+        return multiplication(num1,num2);
+    }
+    else if (operator == "/"){
+        return division(num1,num2);
+    }
+}
+
+console.log(operate(2,3, "/"));
